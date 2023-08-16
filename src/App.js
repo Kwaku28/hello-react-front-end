0689c1b1a344698a207875eb0bf1e7ef03595d8e
Lawrence Amoafo <rekenburg777@gmail.com>
2023-08-16 18:26:04 +0000

    Remove commented codes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Greeting from './components/greeting';
 
-// const App = () => (
-//   <Router>
-//     <Routes>
-//       <Route exact path="/" element={<Greeting />} />
-//     </Routes>
-//   </Router>
-// );
-
 function App() {
   return (
     <Routes>
